f95313c4010827d4f5754a736a3ca5ff37cc5734
txiverke <txiverke@gmail.com>
2018-01-22 10:42:44 -0500

    Added reducers tests

--- src/__tests__/reducers.test.js
+++ src/__tests__/reducers.test.js
@@ -11,4 +11,3 @@ describe('Reducers', () => {
     expect(state).toEqual({statistic:{completed:true,data:{posts:2,users:1,projects:2},message:'Statistics loaded.',error:false},user:{completed:true,data:{bio:'Versatile Front-end Developer with lots of experience in HTML\nand CSS implementation, Cross-browser and Responsive design.\nWide range of professional experience in j avascript using several\nframeworks, libraries and vanilla js. Experience in agile working\nenvironments and Scrum methodology.',firstname:'Xavi',job:'frontend developer',lastname:'Vilà',username:'txiverke'},message:'User data loaded.',error:false},authenticate:{completed:false,data:'',message:'',error:false}});
   });
 })
-
