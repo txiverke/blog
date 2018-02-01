48c4bb3a0f56986194c81f19cf53a70f1b16d335
txiverke <txiverke@gmail.com>
2018-02-01 10:47:54 -0500

    Added more statsActionCreators tests

--- src/actions/__tests__/statsActionCreators.test.js
+++ src/actions/__tests__/statsActionCreators.test.js
@@ -1,21 +1,19 @@
+import request from 'supertest-as-promised'
 import * as action from '../statsActionCreators'
 import * as type from '../actionsType'
-import config from '../../config'
 
 describe('statsActionCreators', () => {
   it('should return LOAD_STATS_REQUEST action', () => {
-    expect(action.loadStatsRequest()).toEqual({ type: type.LOAD_STATS_REQUEST })
+    expect(action.loadStatsRequest()).toMatchSnapshot()
   })
 
   it('should return LOAD_STATS_SUCCESS action', () => {
     const result = { testing: true }
-    expect(action.loadStatsSuccess(result)).toEqual({
-      type: type.LOAD_STATS_SUCCESS,
-      payload: result
-    })
+    expect(action.loadStatsSuccess(result)).toMatchSnapshot()
   })
 
   it('should return LOAD_STATS_FAILURE action', () => {
-    expect(action.loadStatsFailure()).toEqual({ type: type.LOAD_STATS_FAILURE })
+    expect(action.loadStatsFailure()).toMatchSnapshot()
   })
+
 })
