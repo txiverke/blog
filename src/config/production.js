5918350f8eb2fc1235769096dd95758b7919246d
txiverke <txiverke@gmail.com>
2018-02-16 13:44:49 -0500

    Added production config files

--- src/config/production.js
+++ src/config/production.js
@@ -1,7 +1,7 @@
 export const api = {
-  url: process.env.API_URL || 'http://159.203.20.24:8888/api/blog',
-  auth: process.env.AUTH_URL || 'http://159.203.20.24:8888/auth/signin',
-  public: process.env.PUBLIC || 'http://159.203.20.24:8888/blog/img',
-  profileId: process.env.USER_ID || '5a8627b8734d1d1523dd0c40',
-  token: process.env.TOKEN_KEY || 'xavierVilaTechToken'
+  url: process.env.API_URL,
+  auth: process.env.AUTH_URL,
+  public: process.env.PUBLIC,
+  profileId: process.env.USER_ID,
+  token: process.env.TOKEN_KEY,
 }
