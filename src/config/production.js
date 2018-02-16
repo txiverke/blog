8e33bc634e653de52b107385c77bde8f487b8781
txiverke <txiverke@gmail.com>
2018-02-16 15:16:34 -0500

    Added production env vars

--- src/config/production.js
+++ src/config/production.js
@@ -1,7 +1,7 @@
 export const api = {
-  url: process.env.API_URL,
-  auth: process.env.AUTH_URL,
-  public: process.env.PUBLIC,
-  profileId: process.env.USER_ID,
-  token: process.env.TOKEN_KEY,
+  url: process.env.REACT_APP_API_URL,
+  auth: process.env.REACT_APP_AUTH_URL,
+  public: process.env.REACT_APP_PUBLIC,
+  profileId: process.env.REACT_APP_USER_ID,
+  token: process.env.REACT_APP_TOKEN_KEY,
 }
