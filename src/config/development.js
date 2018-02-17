3ecc9827c9a4bcf114d799ea20c9c97f9ac092c0
txiverke <txiverke@gmail.com>
2018-02-17 11:58:18 -0500

    Checking promises

--- src/config/development.js
+++ src/config/development.js
@@ -1,7 +1,7 @@
 export const api = {
-  url: process.env.API_URL || 'http://localhost:8888/api/blog',
-  auth: process.env.AUTH_URL || 'http://localhost:8888/auth/signin',
-  public: process.env.PUBLIC || 'http://localhost:8888/blog/img',
-  profileId: process.env.USER_ID || '5a8617fe734d1d1523dd01fd',
+  url: process.env.API_URL || 'http://localhost:5000/api/blog',
+  auth: process.env.AUTH_URL || 'http://localhost:5000/auth/signin',
+  public: process.env.PUBLIC || 'http://localhost:5000/blog/img',
+  profileId: process.env.USER_ID || '5a8627b8734d1d1523dd0c40',
   token: process.env.TOKEN_KEY || 'xavierVilaTechToken'
 }
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -82,6 +82,7 @@ export const setPromise = {
   set types (val): string { this.type = val },
 
   async response() {
+    console.log(JSON.parse(localStorage.getItem(config.api.token)))
     const headers = new Headers({ 'access_token': JSON.parse(localStorage.getItem(config.api.token)) })
 
     if (this.type) headers.append('Content-Type', this.type)
