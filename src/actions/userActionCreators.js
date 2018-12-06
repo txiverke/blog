7d56df52886ea90e3ae87569930951ee568362d1
txiverke <txiverke@gmail.com>
2018-12-06 12:57:14 -0500

    Update v: 0.1.9 - Fix login action

--- src/actions/userActionCreators.js
+++ src/actions/userActionCreators.js
@@ -1,115 +1,138 @@
 // @flow
 
-import * as ACTION from './actionsType'
-import config from '../config'
-import { handleToken, setPromise } from '../utils/helpers'
+import * as ACTION from './actionsType';
+import config from '../config';
+import { handleToken, setPromise } from '../utils/helpers';
 
-const URL = 'users'
+const URL = 'users';
 
 /** LOGIN USER **/
-export const loginUserRequest = () => ({ type: ACTION.LOG_IN_USER_REQUEST })
-export const loginUserSuccess = (payload: Object) => ({ type: ACTION.LOG_IN_USER_SUCCESS, payload })
-export const loginUserFailure = () => ({ type: ACTION.LOG_IN_USER_FAILURE})
+export const loginUserRequest = () => ({ type: ACTION.LOG_IN_USER_REQUEST });
+export const loginUserSuccess = (payload: Object) => ({
+  type: ACTION.LOG_IN_USER_SUCCESS,
+  payload,
+});
+export const loginUserFailure = () => ({ type: ACTION.LOG_IN_USER_FAILURE });
 
-export const loginUser = (obj: Object) =>
-  async (dispatch: Function) => {
-    dispatch(loginUserRequest())
+export const loginUser = (obj: Object) => async (dispatch: Function) => {
+  dispatch(loginUserRequest());
 
-    try {
-      const promise = await fetch(config.api.auth, { 
-        method: 'POST', 
-        body: JSON.stringify(obj), 
-        headers: new Headers({ 'Content-Type': 'application/json' })
-      })
-      const response = await promise.json()
+  try {
+    const promise = await fetch(config.api.auth, {
+      method: 'POST',
+      body: JSON.stringify(obj),
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+    });
+    
+    const response = await promise.json();
 
-      handleToken.set(JSON.stringify(response.data))
-      dispatch(loginUserSuccess(response.data))
-      return dispatch(isAuthenticatedSuccess(response.data))
-    } catch (err) {
-      return dispatch(loginUserFailure())
-    }
+    if (!response.success) return dispatch(loginUserFailure());
+
+    handleToken.set(JSON.stringify(response.data));
+    dispatch(loginUserSuccess(response.data));
+    return dispatch(isAuthenticatedSuccess(response.data));
+  } catch (err) {
+    return dispatch(loginUserFailure());
   }
+};
 
 /** AUTHENTICATE USER  */
-export const isAuthenticatedRequest = () => ({ type: ACTION.IS_AUTHENTICATED_REQUEST })
-export const isAuthenticatedSuccess = (payload: string) => ({ type: ACTION.IS_AUTHENTICATED_SUCCESS, payload })
-export const isAuthenticatedFailure = () => ({ type: ACTION.IS_AUTHENTICATED_FAILURE})
+export const isAuthenticatedRequest = () => ({
+  type: ACTION.IS_AUTHENTICATED_REQUEST,
+});
+export const isAuthenticatedSuccess = (payload: string) => ({
+  type: ACTION.IS_AUTHENTICATED_SUCCESS,
+  payload,
+});
+export const isAuthenticatedFailure = () => ({
+  type: ACTION.IS_AUTHENTICATED_FAILURE,
+});
 
-export const isAuthenticated = () => 
-  (dispatch: Function) => {
-    const token = handleToken.get()
-    dispatch(isAuthenticatedRequest())
-
-    if (token) {
-      return dispatch(isAuthenticatedSuccess(token))
-    }
-
-    return dispatch(isAuthenticatedFailure())
-  }
-
-/** LOGOUT USER */
-export const logoutUserRequest = () => ({ type: ACTION.LOG_OUT_USER_REQUEST })
-export const logoutUserSuccess = () => ({ type: ACTION.LOG_OUT_USER_SUCCESS })
-export const logoutUserFailure = () => ({ type: ACTION.LOG_OUT_USER_FAILURE })
-
-export const logoutUser = () => (dispatch: Function) => {
-  const token = handleToken.get()
-  dispatch(logoutUserRequest())
+export const isAuthenticated = () => (dispatch: Function) => {
+  const token = handleToken.get();
+  dispatch(isAuthenticatedRequest());
 
   if (token) {
-    handleToken.remove()
-    dispatch(logoutUserSuccess())
-    return dispatch((isAuthenticated()))
+    return dispatch(isAuthenticatedSuccess(token));
   }
 
-  return dispatch(logoutUserFailure())
-}
+  return dispatch(isAuthenticatedFailure());
+};
+
+/** LOGOUT USER */
+export const logoutUserRequest = () => ({ type: ACTION.LOG_OUT_USER_REQUEST });
+export const logoutUserSuccess = () => ({ type: ACTION.LOG_OUT_USER_SUCCESS });
+export const logoutUserFailure = () => ({ type: ACTION.LOG_OUT_USER_FAILURE });
+
+export const logoutUser = () => (dispatch: Function) => {
+  const token = handleToken.get();
+  dispatch(logoutUserRequest());
+
+  if (token) {
+    handleToken.remove();
+    dispatch(logoutUserSuccess());
+    return dispatch(isAuthenticated());
+  }
+
+  return dispatch(logoutUserFailure());
+};
 
 /** LOAD USER DATA **/
-export const loadUserDataRequest = () => ({ type: ACTION.LOAD_USER_DATA_REQUEST })
-export const loadUserDataSuccess = (payload: Object) => ({ type: ACTION.LOAD_USER_DATA_SUCCESS, payload })
-export const loadUserDataFailure = () => ({ type: ACTION.LOAD_USER_DATA_FAILURE })
+export const loadUserDataRequest = () => ({
+  type: ACTION.LOAD_USER_DATA_REQUEST,
+});
+export const loadUserDataSuccess = (payload: Object) => ({
+  type: ACTION.LOAD_USER_DATA_SUCCESS,
+  payload,
+});
+export const loadUserDataFailure = () => ({
+  type: ACTION.LOAD_USER_DATA_FAILURE,
+});
 
-export const loadUserData = (id: string) =>
-  async (dispatch: Function) => {
-    dispatch(loadUserDataRequest())
+export const loadUserData = (id: string) => async (dispatch: Function) => {
+  dispatch(loadUserDataRequest());
 
-    try {
-      // $FlowFixMe
-      setPromise.method = 'GET'
-      // $FlowFixMe
-      setPromise.body = null
-      // $FlowFixMe
-      setPromise.urls = `${URL}/${id}`
-      const data = await setPromise.response()
-      return dispatch(loadUserDataSuccess(data))
-    } catch (err) {
-      return dispatch(loadUserDataFailure())
-    }
+  try {
+    // $FlowFixMe
+    setPromise.method = 'GET';
+    // $FlowFixMe
+    setPromise.body = null;
+    // $FlowFixMe
+    setPromise.urls = `${URL}/${id}`;
+    const data = await setPromise.response();
+    return dispatch(loadUserDataSuccess(data));
+  } catch (err) {
+    return dispatch(loadUserDataFailure());
   }
+};
 
 /** UPDATE USER DATA **/
-export const updateUserDataRequest = () => ({ type: ACTION.UPDATE_USER_DATA_REQUEST })
-export const updateUserDataSuccess = (payload: Object) => ({ type: ACTION.UPDATE_USER_DATA_SUCCESS, payload })
-export const updateUserDataFailure = () => ({ type: ACTION.UPDATE_USER_DATA_FAILURE })
+export const updateUserDataRequest = () => ({
+  type: ACTION.UPDATE_USER_DATA_REQUEST,
+});
+export const updateUserDataSuccess = (payload: Object) => ({
+  type: ACTION.UPDATE_USER_DATA_SUCCESS,
+  payload,
+});
+export const updateUserDataFailure = () => ({
+  type: ACTION.UPDATE_USER_DATA_FAILURE,
+});
 
-export const updateUserData = (obj: Object) =>
-  async (dispatch: Function) => {
-    dispatch(updateUserDataRequest())
+export const updateUserData = (obj: Object) => async (dispatch: Function) => {
+  dispatch(updateUserDataRequest());
 
-    try {
-      // $FlowFixMe
-      setPromise.method = 'PUT'
-      // $FlowFixMe
-      setPromise.body = JSON.stringify(obj)
-      // $FlowFixMe
-      setPromise.urls = `${URL}/${config.api.profileId}`
-      // $FlowFixMe
-      setPromise.types = 'application/json'
-      const data = await setPromise.response()
-      return dispatch(updateUserDataSuccess(data))
-    } catch (err) {
-      return dispatch(updateUserDataFailure())
-    }
+  try {
+    // $FlowFixMe
+    setPromise.method = 'PUT';
+    // $FlowFixMe
+    setPromise.body = JSON.stringify(obj);
+    // $FlowFixMe
+    setPromise.urls = `${URL}/${config.api.profileId}`;
+    // $FlowFixMe
+    setPromise.types = 'application/json';
+    const data = await setPromise.response();
+    return dispatch(updateUserDataSuccess(data));
+  } catch (err) {
+    return dispatch(updateUserDataFailure());
   }
+};
